a1574e72ef2121c3ad37b72cf6450a681cbece72
BRUNO PILAO DA ROCHA <202201037911@alunos.ibmec.edu.br>
2024-10-17 20:34:56 -0300

    Implementação de Tela de Pause

---
+++ just-keep-alive/Script/gameplay.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):  # Verifica se a tecla "Esc" foi pressionada
+		get_tree().change_scene_to_file("res://Scenes/pause_screen.tscn")
--- just-keep-alive/Script/pause_screen.gd
+++ just-keep-alive/Script/pause_screen.gd
@@ -2,13 +2,18 @@ extends CanvasLayer
 
 # Chama quando o nó entra na árvore de cena
 func _ready() -> void:
-	return
-# Função chamada ao pressionar o botão de "continuar"
+	get_tree().paused
+
 func _on_resume_button_pressed() -> void:
-	return
-# Função chamada ao pressionar o botão de "reiniciar"
-func _on_restart_button_pressed() -> void:
-	return
-# Função chamada ao pressionar o botão de "sair"
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/gameplay.tscn")
+	
 func _on_surrender_button_pressed() -> void:
-	return
+	get_tree().change_scene_to_file("res://Scenes/tela_inicial.tscn")
+
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false  # Certifique-se de que o jogo não está pausado
+	get_tree().change_scene_to_file("res://Scenes/gameplay.tscn")
+	get_tree().reload_current_scene()  # Recarrega a cena de gameplay
