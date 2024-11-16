195461b785d0dfb811ccdb4da3fd99a57f14266c
brunopdrocha <brunopdrocha@gmail.com>
2024-11-16 19:45:54 -0300

    Botao de Pause corrigido e funcional

--- just-keep-alive/Script/pause_screen.gd
+++ just-keep-alive/Script/pause_screen.gd
@@ -3,9 +3,10 @@ extends CanvasLayer
 func _ready() -> void:
 	visible = false # Menu de pausa começa invisível
 
+
 func _process(delta):
 	# Verifica se a tecla de "cancelar" (Esc) foi pressionada
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("pause"):
 		if visible:
 			# Se o menu de pausa estiver visível, despausa o jogo e esconde o menu
 			visible = false
